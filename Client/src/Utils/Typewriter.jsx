07c406784d2597415ea093a02bce168c229d8ce8
itamarkraus7 <itamarkraus7@gmail.com>
2024-03-23 13:13:56 +0200

    fix conflicts1

---
+++ Client/src/Utils/Typewriter.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+export const Typewriter = ({ text, speed }) => {
+  const [displayText, setDisplayText] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    if (currentIndex < text.length) {
+      const timer = setTimeout(() => {
+        setDisplayText((prevText) => prevText + text[currentIndex]);
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+      }, speed);
+
+      return () => clearTimeout(timer);
+    }
+  }, [currentIndex, speed, text]);
+
+  return <span>{displayText}</span>;
+};
