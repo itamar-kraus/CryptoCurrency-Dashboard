f6963d4a04f1e111938ba2ca9aaf9d0875fa5f98
TomerLazarovitch7 <tomer.lazarovitch@e.braude.ac.il>
2024-03-24 11:14:43 +0200

    fixed news style. Added color to the logo. In contact: clear form after succesfuly submit. Added arrow icon to FAQ and NEWS.

--- Client/src/pages/Contact/Contact.jsx
+++ Client/src/pages/Contact/Contact.jsx
@@ -20,6 +20,9 @@ export const Contact = () => {
       if (response.status >= 200 && response.status < 300) {
         console.log(response.data.message); // Log the response message from the server
         alert(`${name}, thank you for sending us a message!`);
+        setName("");
+        setEmail("");
+        setMessage("");
       } else {
         console.error("Failed to insert data into the database");
       }
@@ -135,8 +138,7 @@ export const Contact = () => {
           </div>
           <button
             type="submit"
-            style={{ border: "1px solid white" }}
-            class="px-4 py-2 rounded transition-all hover:bg-blue-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-gray-400 focus:ring-opacity-50"
+            class="px-4 py-2 rounded transition-all hover:bg-blue-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-gray-400 focus:ring-opacity-50 border border-blue-gray-200 bg-transparent font-sans text-sm font-normal text-blue-gray-700"
           >
             Send Message
           </button>
@@ -147,65 +149,3 @@ export const Contact = () => {
 };
 
 export default Contact;
-//   return (
-//     <div className="tab-content font-bold text-gray-800 dark:text-white">
-//       <p className="text-xl sm:text-3xl">
-//         Have questions or feedback? We'd love to hear from you!
-//       </p>
-//       <div id="contact-details" className="text-gray-800 dark:text-white">
-//         <strong className="text-xl sm:text-2xl">Email: </strong>
-//         <p>info@b1_crypto.com</p>
-//         <strong className="text-xl sm:text-2xl">Phone: </strong>
-//         <p>+1 (555) 123-4567</p>
-//       </div>
-//         <div className="form-container">
-//           <div className="contact-form">
-//             <h2 className="headline text-center text-2xl text-gray-800 dark:text-white">
-//               Send us a Message
-//             </h2>
-//             <form onSubmit={handleSubmit}>
-//               <input
-//                 type="text"
-//                 id="name"
-//                 name="name"
-//                 value={name}
-//                 onChange={(e) => setName(e.target.value)}
-//                 placeholder="Your Name"
-//                 required
-//                 className="dark:bg-white dark:text-white dark:border-gray-600"
-//               />
-//               <input
-//                 type="email"
-//                 id="email"
-//                 name="email"
-//                 value={email}
-//                 onChange={(e) => setEmail(e.target.value)}
-//                 placeholder="Your Email"
-//                 required
-//                 className="dark:bg-white dark:text-white dark:border-gray-600"
-//               />
-//               <textarea
-//                 rows="3"
-//                 id="message"
-//                 name="message"
-//                 value={message}
-//                 onChange={(e) => setMessage(e.target.value)}
-//                 placeholder="Your Message"
-//                 required
-//                 className="dark:bg-white dark:text-white dark:border-gray-600"
-//               />
-//               <button
-//                 type="submit"
-//                 style={{ border: "1px solid black" }}
-//                 className="px-4 py-2 rounded dark:bg-gray-800 dark:text-white dark:border-gray-700"
-//               >
-//                 Send Message
-//               </button>
-//             </form>
-//           </div>
-//         </div>
-//     </div>
-//   );
-// };
-
-// export default Contact;
--- Client/src/pages/News/News.jsx
+++ Client/src/pages/News/News.jsx
@@ -1,73 +1,176 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
+
+// export const News = () => {
+//     const [newsArray, setNewsArray] = useState([]);
+
+//     useEffect(() => {
+//         const fetchNews = async () => {
+//             try {
+//                 const response = await fetch('http://localhost:3000/News');
+//                 if (!response.ok) {
+//                     throw new Error('Failed to fetch news');
+//                 }
+//                 const data = await response.json();
+//                 const news = [];
+//                 data.forEach(category => {
+//                     Object.keys(category).forEach(categoryName => {
+//                         if (categoryName !== "_id") {
+//                             news.push({
+//                                 title: categoryName,
+//                                 link: null
+//                             });
+//                         }
+//                         if (Array.isArray(category[categoryName])) {
+//                             category[categoryName].forEach(newsItem => {
+//                                 news.push({
+//                                     title: newsItem.title,
+//                                     link: newsItem.link
+//                                 });
+//                             });
+//                         }
+//                     });
+//                 });
+//                 setNewsArray(news);
+//             } catch (error) {
+//                 console.error('Error fetching data:', error);
+//             }
+//         };
+
+//         fetchNews();
+//     }, []);
+
+//     const renderNews = (text, link) => {
+//         return (
+//             <div>
+//                 <h3 className={link ? "text-xl font-semibold" : "text-xl font-bold text-blue-500"}>
+//                     {text}
+//                     {link && <a href={link} className="text-blue-500 font-bold">Read More</a>}
+//                 </h3>
+//             </div>
+//         );
+//     };
+
+//     return (
+//         <div className="tab-content text-center" id="tab-News">
+//             <h2 className="text-2xl font-bold mb-4">Get the Latest Crypto News Here</h2>
+//             <p className="mb-8">Stay informed with our latest updates and crypto articles.</p>
+//             <div id="news-list">
+//                 {newsArray.map((newsItem, index) => (
+//                     <div key={index}>
+//                         {renderNews(newsItem.title, newsItem.link)}
+//                     </div>
+//                 ))}
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default News;
+
+import React, { useState, useEffect } from "react";
 
 export const News = () => {
-    const [newsArray, setNewsArray] = useState([]);
+  const [newsArray, setNewsArray] = useState([]);
+  const [openIndex, setOpenIndex] = useState(null); // Track the index of the opened item
 
-    useEffect(() => {
-        const fetchNews = async () => {
-            try {
-                const response = await fetch('http://localhost:3000/News');
-                if (!response.ok) {
-                    throw new Error('Failed to fetch news');
-                }
-                const data = await response.json();
-                const news = [];
-                data.forEach(category => {
-                    Object.keys(category).forEach(categoryName => {
-                        if (categoryName !== "_id") {
-                            news.push({
-                                title: categoryName,
-                                link: null
-                            });
-                        }
-                        if (Array.isArray(category[categoryName])) {
-                            category[categoryName].forEach(newsItem => {
-                                news.push({
-                                    title: newsItem.title,
-                                    link: newsItem.link
-                                });
-                            });
-                        }
-                    });
-                });
-                setNewsArray(news);
-            } catch (error) {
-                console.error('Error fetching data:', error);
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/News");
+        if (!response.ok) {
+          throw new Error("Failed to fetch news");
+        }
+        const data = await response.json();
+        const news = [];
+        data.forEach((category) => {
+          Object.keys(category).forEach((categoryName) => {
+            if (categoryName !== "_id") {
+              news.push({
+                title: categoryName,
+                link: null,
+              });
             }
-        };
-
-        fetchNews();
-    }, []);
-
-    const renderNews = (text, link) => {
-        return (
-            <div>
-                <h3 className={link ? "text-xl font-semibold" : "text-xl font-bold text-blue-500"}>
-                    {text}
-                    {link && <a href={link} className="text-blue-500 font-bold">Read More</a>}
-                </h3>
-            </div>
-        );
+            if (Array.isArray(category[categoryName])) {
+              category[categoryName].forEach((newsItem) => {
+                news.push({
+                  title: newsItem.title,
+                  link: newsItem.link,
+                });
+              });
+            }
+          });
+        });
+        setNewsArray(news);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
 
-    return (
-        <div className="tab-content text-center" id="tab-News">
-            <h2 className="text-2xl font-bold mb-4">Get the Latest Crypto News Here</h2>
-            <p className="mb-8">Stay informed with our latest updates and crypto articles.</p>
-            <div id="news-list">
-                {newsArray.map((newsItem, index) => (
-                    <div key={index}>
-                        {renderNews(newsItem.title, newsItem.link)}
-                    </div>
-                ))}
+    fetchNews();
+  }, []);
+
+  const toggleNewsItem = (index) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
+
+  const noArrowTitles = ["BTC_news", "ETH_news", "Other_news"];
+
+  return (
+    <div className="tab-content  text-gray-800 dark:text-white">
+      <h2 className="center-align text-4xl font-bold mb-4 bg-[radial-gradient(circle_at_bottom_left,_var(--tw-gradient-stops))] from-purple-800 via-purple-500 to-purple-800 text-transparent bg-clip-text">
+        Crypto News
+      </h2>
+      <p className="mb-8 text-2xl">
+        Stay informed with the latest updates and crypto articles.
+      </p>
+      <div id="news-list">
+        {newsArray.map((newsItem, index) => (
+          <div key={index} className="mb-4">
+            <div
+              className="flex items-center cursor-pointer"
+              onClick={() => toggleNewsItem(index)}
+            >
+              <h3
+                className={`text-2xl ${
+                  noArrowTitles.includes(newsItem.title)
+                    ? "text-purple-300"
+                    : "text-gray-800 dark:text-white mr-2"
+                }`}
+              >
+                {newsItem.title}
+              </h3>
+              {/* Only render the SVG arrow if the title does not match the specified no-arrow titles */}
+              {!noArrowTitles.includes(newsItem.title) && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className={`w-6 h-6 transform transition-transform duration-300 ${
+                    openIndex === index ? "rotate-180" : ""
+                  }`}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                  />
+                </svg>
+              )}
             </div>
-        </div>
-    );
+            {openIndex === index && newsItem.link && (
+              <p className="text-gray-600 text-xl dark:text-white mt-2">
+                <a href={newsItem.link} className="text-blue-500">
+                  Read More
+                </a>
+              </p>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default News;
-
-
-
-
-
