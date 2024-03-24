88fb01b7d50fc9b80ef027481bc73687a2b0dfa0
itamarkraus7 <itamarkraus7@gmail.com>
2024-03-24 21:22:49 +0200

    ssl

--- Client/src/Config/config.js
+++ Client/src/Config/config.js
@@ -1,8 +1,10 @@
 const DEFAULT_HOST = 'localhost';
 const DEFAULT_PORT = 4000;
 
-const host = process.env.HOST || DEFAULT_HOST;
-const port = process.env.PORT || DEFAULT_PORT;
+// const host = process.env.HOST || DEFAULT_HOST;
+// const port = process.env.PORT || DEFAULT_PORT;
+const host = 'crypto-currency-dashboard-server.vercel.app';
+const port = '443';
 
 module.exports = {
   host,
